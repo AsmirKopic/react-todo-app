9cc3054c2435fa939c2305fbe48c4395aa7ab692
unknown <asmir.kopic123@gmail.com>
2022-06-27 23:11:19 +0200

    added some styles and testing with time

--- src/components/App.js
+++ src/components/App.js
@@ -43,6 +43,30 @@ function App() {
     setTodos(newTodos)
   }
 
+
+  // testing with time change
+  const time = new Date();
+  const curretTime = time.getHours();
+
+  let msg;
+  
+  const customStyle = {
+    color: ""
+  }
+
+  if (curretTime < 12) {
+    msg = "Good morning"
+    customStyle.color = "blue"
+  } else if (curretTime > 21) {
+    msg = "Good night!!"
+    customStyle.color = "red"
+  } else {
+    msg = "Good DAY"
+    customStyle.color = "yellow"
+  }
+
+  console.log(curretTime)
+
   return (
     <>
       <TodoList todos = {todos} toggleTodo={toggleTodo} />
@@ -52,6 +76,10 @@ function App() {
       <div>
         {todos.filter(todo => !todo.complete).length} left to do.
       </div>
+
+      <img className="todo-img" alt='todo' src='https://camo.githubusercontent.com/c43d969d9d071c8342e9a69cdd6acb433c541f431127738974ce22290c46f2b8/68747470733a2f2f692e696d6775722e636f6d2f4f764d5a4273392e6a7067'/>
+
+      <h1 style={customStyle}>{msg}</h1>
     </>
   );
 }
